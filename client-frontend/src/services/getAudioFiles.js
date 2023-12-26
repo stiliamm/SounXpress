9468709e8afe0cbef6564677114d1b3b27856e71
stiliamm <redemption.records@icloud.com>
2023-12-26 22:17:49 +0200

    feature: getting audio from server

--- client-frontend/src/services/getAudioFiles.js
+++ client-frontend/src/services/getAudioFiles.js
@@ -1,20 +1,25 @@
-export const GetAudioData = async() => {
-    try {
-        const response = await fetch('localhost:8000/audio_gallery', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+export const GetAudioData = async (authToken) => {
+  try {
+    const response = await fetch("http://localhost:8000/users/recordings", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "x-token": authToken,
+      },
+    });
 
-        if (!response.ok) {
-            throw new Error('Network was not ok!')
-        }
-
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        throw error;
+    if (response.status === 401) {
+      return 401;
     }
+
+    if (!response.ok) {
+      throw new Error("Network was not ok!");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
 };
---
+++ client-frontend/src/services/getPlayback.js
@@ -0,0 +1,28 @@
+export const GetPlayback = async (username, file_name, authToken) => {
+  try {
+    const response = await fetch(
+      `http://localhost:8000/users/recordings/play_from_dir?username=${username}&file_name=${file_name}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-token": authToken,
+        },
+      }
+    );
+
+    if (response.status === 401) {
+      return 401;
+    }
+
+    if (!response.ok) {
+      throw new Error("Network was not ok!");
+    }
+
+    const data = await response.blob();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
